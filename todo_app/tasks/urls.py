555d745f4892a2b25f9ba17abc8e5ec58449e311
daniil <daniilviking@gmail.com>
2023-01-05 22:26:45 +0200

    changes urlspatterns

--- todo_app/tasks/urls.py
+++ todo_app/tasks/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-from rest_framework.routers import DefaultRouter
-
 from .views import TaskView, TaskDetailView
 
 app_name = 'tasks'
 
-# router = DefaultRouter()
-# router.register("tasks", TaskView, "tasks")
-# router.register("tasks/<int:pk>/", TaskDetailView, "tasks_detail")
-# urlpatterns = [
-#               ] + router.get_urls()
-
 urlpatterns = [
-    # code omitted for brevity
     path('tasks/', TaskView.as_view()),
     path('tasks/<int:pk>/', TaskDetailView.as_view()),
 ]
